400acae900003cbcf9dc245f7e9307278b39de05
cartoon-raccoon <shaughn.chan@gmail.com>
2020-09-27 15:55:07 +0800

    Added main structure of lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,43 +1,102 @@
-pub enum Token {
+#[derive(Clone, Copy, Debug)]
+pub enum TokenType {
+    //core keywords
     Struct,
     Function,
+    Method,
     Trait,
     Closure,
     Impl,
     Enum,
+    Return,
+    This,
+
+
+    //control flow and logic
     If,
     Else,
-    Nil,
-    Int,
-    Float,
+    Match,
+    And,        // &&
+    Or,         // ||
+    True,
+    False,
+    While,
+    For,
+
+    //error handling
     Try,
     Catch,
+    Finally,
+
+    //types and identifiers
+    Ident,
+    Strng,
+    Int,
+    Float,
+    Nil,
+
+    //operators
+    Assgn,      // =
+    Plus,       // +
+    Minus,      // -
+    Times,      //\*
+    Divide,     // /
+    Eq,         // ==
+    Neq,        //\!=
+    GEq,        // >=
+    LEq,        // <=
+
+    //delimiters
     StmtEnd,    // ;
-    OpenCBkt,   // (
-    CloseCBkt,  // )
-    OpenSqBkt,  // [
-    CloseSqBkt, // ]
+    LeftCBkt,   // (
+    RightCBkt,  // )
+    LeftSqBkt,  // [
+    RightSqBkt, // ]
     OpenBlock,  // {
     CloseBlock, // }
+
+    //tokens that throw an error
     Unknown,    // Exits with an error if encountered
 }
 
+impl From<TokenType> for String {
+    fn from(t: TokenType) -> String {
+        String::new() //TODO: Implement this
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Token {
+    tokentype: TokenType,
+    lexeme: String,
+    line: u64,
+    column: u64,
+}
+
+impl Token {
+    pub fn to_string(&self) -> String {
+        String::from(self.tokentype.clone()) //might need to clone here in the future
+    } 
+}
+
+#[derive(Clone, Debug)]
 pub struct Lexer {
-    current: u64,
+    current_char: u64,
+    tokens: Vec<Token>,
 }
 
 impl Lexer {
-    pub fn new() {
+    pub fn new() -> Self {
         Lexer {
-            current: 0,
+            current_char: 0,
+            tokens: Vec::new(),
         }
     }
 
-    pub fn scan(&self, input: String) { //use String or &str?
+    pub fn scan(&self, input: String) -> Vec<Token> {
+        for c in input.chars() {
 
+        }
+        Vec::new()
     }
-}
-
-pub struct Parser {
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod lexer;
+
+use lexer::Lexer;
+
 fn main() {
+    let scanner = Lexer::new();
     println!("Welcome to Verdigris!");
 }
