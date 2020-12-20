a606c186260cb6873da5d1b7ebbf37576c23301f
cartoon-raccoon <shaughn.chan@gmail.com>
2020-12-21 00:35:29 +0800

    Added more more opcode implementations, factored out one stepthrough

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,19 +1,22 @@
 #[non_exhaustive]
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Opcode {
-    Hlt, // Halt
-    Add, // Add
-    Sub, // Subtract
+    Hlt, // Halt execution
+    Mov, // Load register
     Jmp, // Jump to a location in program
     Jeq, // Jump if equal
     Jne, // Jump if not equal
+    Add, // Add
+    Sub, // Subtract
     Igl, // Illegal
 }
 
 impl From<u8> for Opcode {
     fn from(from: u8) -> Self {
         match from {
-            0x06 => Opcode::Hlt,
+            0x00 => Opcode::Hlt,
+            0x01 => Opcode::Mov,
+            0x07 => Opcode::Add,
             _ => Opcode::Igl,
         }
     }
@@ -36,11 +39,15 @@ mod tests {
     use super::*;
     #[test]
     fn test_opcode_from() {
-        let hlt = 0x06;
-        let igl = 0x01;
+        let hlt = 0x00;
+        let mov = 0x01;
+        let igl = 0xf7;
+
         let hltopcode = Opcode::from(hlt);
+        let movopcode = Opcode::from(mov);
         let iglopcode = Opcode::from(igl);
         assert_eq!(hltopcode, Opcode::Hlt);
+        assert_eq!(movopcode, Opcode::Mov);
         assert_eq!(iglopcode, Opcode::Igl);
     }
 }
