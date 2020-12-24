c10429c86c520c9c16e92735650f9c578bafebed
cartoon-raccoon <shaughn.chan@gmail.com>
2020-12-25 01:31:05 +0800

    Added directive type to assembler

--- vdg-oxidizer/src/assembler/mod.rs
+++ vdg-oxidizer/src/assembler/mod.rs
@@ -1,10 +1,11 @@
 pub mod assembler;
 pub mod lexer;
+pub mod parser;
 
 pub use assembler::Assembler;
-pub use lexer::Lexer;
+pub use lexer::{Lexer, Token, Context};
+pub use parser::{Parser, Operand};
 
-use lexer::Context;
 use std::fmt;
 
 #[derive(Debug, Clone, PartialEq)]
@@ -15,6 +16,9 @@ pub enum AsmParseErr {
     IncorrectOperandNo(u8, u8, Context),
     InvalidRegister(u32, Context),
     CouldNotParse(String, Context),
+    InvalidDirective(String, Context),
+    InvalidOperand(Operand, Context),
+    InvalidOperandConversion(Token),
 }
 
 impl std::error::Error for AsmParseErr {}
@@ -47,7 +51,7 @@ impl fmt::Display for AsmParseErr {
             }
             Self::InvalidRegister(num, con) => {
                 write!(f, 
-                    "Error: invalid register {}\nLine {}, Column {}", 
+                    "Error: invalid register {}\nLine {} Column {}", 
                     num, con.line, con.column
                 )
             }
@@ -56,6 +60,54 @@ impl fmt::Display for AsmParseErr {
                     "Error: could not parse `{}` as a number\nLine {} Column {}", 
                     text, con.line, con.column
                 )
+            }
+            Self::InvalidDirective(dir, con) => {
+                write!(f, 
+                    "Error: invalid directive {}\nLine {} Column {}",
+                    dir, con.line, con.column
+                )
+            }
+            Self::InvalidOperand(op, con) => {
+                write!(f,
+                    "Error: invalid operand {:?}\nLine {} Column {}",
+                    op, con.line, con.column
+                )
+            }
+            Self::InvalidOperandConversion(token) => {
+                write!(f,
+                    "Error: invalid operand conversion {}\nLine {} Column {}",
+                    token, token.context().line, token.context().column
+                )
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Directive {
+    Code,
+    Data,
+    String,
+    Global,
+}
+
+impl Directive {
+    pub fn try_from(from: String, con: Context) -> Result<Self, AsmParseErr> {
+        match from.as_str() {
+            "code" => {
+                Ok(Self::Code)
+            }
+            "data" => {
+                Ok(Self::Data)
+            }
+            "string" => {
+                Ok(Self::String)
+            }
+            "global" => {
+                Ok(Self::Global)
+            }
+            inval @ _ => {
+                Err(AsmParseErr::InvalidDirective(inval.to_string(), con))
             }
         }
     }
