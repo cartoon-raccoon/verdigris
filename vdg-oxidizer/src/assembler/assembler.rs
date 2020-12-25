92c9f06f8036b2920903627dd3fa14650d85aba0
cartoon-raccoon <shaughn.chan@gmail.com>
2020-12-25 22:23:04 +0800

    Updated assembler

--- vdg-oxidizer/src/assembler/assembler.rs
+++ vdg-oxidizer/src/assembler/assembler.rs
@@ -1,13 +1,62 @@
 use std::collections::HashMap;
 
+use crate::assembler::parser::Parsed;
+
 use crate::instruction::Instruction;
 
+#[derive(Debug, Clone)]
 pub struct Assembler {
     phase: Phase,
-    symbols: HashMap<String, Instruction>,
+    program: Vec<Parsed>,
+    symbols: HashMap<String, Symbol>,
 }
 
+impl Assembler {
+    pub fn new() -> Self {
+        Assembler {
+            phase: Phase::First,
+            program: Vec::new(),
+            symbols: HashMap::new(),
+        }
+    }
+    pub fn assemble(&mut self) -> Vec<u8> {
+        self.first_phase().unwrap();
+        self.phase = Phase::Second;
+        self.second_phase()
+    }
+
+    fn first_phase(&mut self) -> Result<(), ()> {
+        // do stuff
+        unimplemented!()
+    }
+
+    fn second_phase(&mut self) -> Vec<u8> {
+        unimplemented!()
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 enum Phase {
     First,
     Second,
+}
+
+#[derive(Debug, Clone)]
+pub enum AsmInst {
+    Instruction(Instruction),
+    Directive,
+    Label,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Label {
+    Code,
+    Data,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Symbol {
+    name: String,
+    kind: Label,
+    data: Vec<Parsed>
 }
